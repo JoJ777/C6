2b80030a5e4124285f0dbcb31b14c3183e0d8ffb
iEdHu <huan239@foxmail.com>
2023-03-11 09:28:40 +0800

    ch05
    提交ch05章

---
+++ ch05/05_06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("integer division: 5/4 = %d\n", 5 / 4);
+    printf("integer division: 6/3 = %d\n", 6 / 3);
+    printf("integer division: 7/4 = %d\n", 7 / 4);
+    printf("floating division: 7./4. = %1.2f\n", 7. / 4.);
+    printf("mixed division: 7./4 = %1.2f\n", 7. / 4);
+    
+    return 0;
+}
